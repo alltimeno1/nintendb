8c1596fe9651816a46f753ef0bb69fab0e00b573
Sungjun Kim <hzcd108@gmail.com>
2022-03-18 18:38:55 +0900

    다양한 기능 추가
    댓글 등록 버튼 활성화 기능 추가
    BGM 재생 기능 추가
    마리오 지도 이미지 수정

--- functions/reply.js
+++ functions/reply.js
@@ -1,12 +1,15 @@
+
+
 // 댓글 작성
 function writeMsg() {
   const name = $('#nickname').val();
   const msg = $('#msg').val();
+
   if (!name || !msg) { // 닉네임이나 메세지 값이 없을 때
     alert("닉네임과 내용을 입력해주세요!!");
     return
   }
-  let temp_html = `
+  const temp_html = `
     <div class="card">
         <div class="card-body">
         <blockquote class="blockquote mb-0">
@@ -18,6 +21,21 @@ function writeMsg() {
     $(`#cards-box`).append(temp_html);
     alert("좋은 의견 감사합니다!");
 }
+
+// 아이디 비밀번호 입력시 버튼색 변경
+function activate() {
+  const name = $('#nickname').val();
+  const msg = $('#msg').val();
+
+  if (name && msg) {
+    $('.btn-dark').css('background-color', '#1BBC9B');
+  } else {
+    $('.btn-dark').css('background-color', '#212529');
+  }
+}
+
+$('#nickname').on('input', activate);
+$('#msg').on('input', activate);
 
 const btnReply = document.querySelector('.btn-dark');
 
