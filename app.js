95a1b5040efa35175b8dafd54c70bc655d46b2db
Sungjun Kim <hzcd108@gmail.com>
2022-04-08 19:46:57 +0900

    상세페이지 추가 및 폴더명 변경
    PUG를 활용하여 게임 상세페이지 구현
    src 폴더명을 bgm으로 변경

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ const { Title } = db
 app.use(express.json())
 app.use(express.static(__dirname))
 
+app.set('views', 'html')
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => res.send('URL should contain /home'))
-
-app.get('/home', async (req, res) => {
-  res.sendFile(__dirname + '/html/index.html')
-})
-
-app.get('/rank', async (req, res) => {
-  res.send('아직 준비 중입니다. 조금만 기다려 주세요 :(')
-})
 
 app.get('/:page', async (req, res) => {
   const { page } = req.params
-  res.sendFile(__dirname + `/html/${page}.html`)
+  if (page === 'home') {
+    res.sendFile(__dirname + '/html/index.html')
+  } else if (page === 'rank') {
+    res.send('아직 준비 중입니다. 조금만 기다려 주세요 :(')
+  } else {
+    res.sendFile(__dirname + `/html/${page}.html`)
+  }
 })
 
 app.get('/title/:id', async (req, res) => {
@@ -26,14 +27,24 @@ app.get('/title/:id', async (req, res) => {
   const title = await Title.findOne({ where: { id } })
 
   if (title) {
-    res.send(title)
+    const { name, image, date, rating, discount } = title
+
+    res.render('title_info', {
+      name,
+      image,
+      date,
+      rating,
+      discount,
+    })
   } else {
     res
       .status(404)
-      .send({ message: 'There is no title with the id or DB error :(' })
+      .send({ message: 'There is no title with the id or DB disconnected :(' })
   }
 })
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server is listening...')
 })
+
+module.exports = app
